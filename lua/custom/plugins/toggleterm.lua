397ec8bf7cf3d79a453dad17aa92ce1567c498f7
Jan Rossbach <janrossbach3@gmail.com>
2024-03-27 22:12:35 +0000

    fix: Lazygit toggleterm binding

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -15,14 +15,15 @@ return {
           end
         end,
       }
-      -- local Terminal = require('toggleterm.terminal').Terminal
-      -- local lazygit = Terminal:new {
-      --   cmd = 'lazygit',
-      --   hidden = false,
-      --   autochdir = true,
-      --   direction = 'float',
-      --   count = 7,
-      -- }
+      local Terminal = require('toggleterm.terminal').Terminal
+      local lazygit = Terminal:new {
+        cmd = 'lazygit',
+        hidden = true,
+        close_on_exit = true,
+        autochdir = true,
+        direction = 'float',
+        count = 7,
+      }
       -- local ollama = Terminal:new {
       --   cmd = 'ollama run mistral',
       --   hidden = true,
@@ -30,14 +31,20 @@ return {
       --   count = 8,
       -- }
 
-      -- function _lazygit_toggle()
-      --   lazygit:toggle()
-      -- end
+      function _lazygit_toggle()
+        if lazygit:is_open() then
+          lazygit:close()
+        else
+          lazygit:open()
+          vim.cmd 'startinsert!'
+        end
+      end
       --
       -- function _ollama_toggle()
       --   ollama:toggle()
       -- end
 
+      vim.keymap.set('n', '<leader>gl', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
       vim.keymap.set('n', '<C-CR>', '<cmd>exe v:count1 . "ToggleTerm"<CR>i', { noremap = true, silent = true, desc = 'Toggle Floating Terminal' })
       vim.keymap.set('t', '<C-CR>', '<C-\\><C-n><cmd>exe v:count1 . "ToggleTerm"<CR>', { noremap = true, silent = true, desc = 'Toggle Floating Terminal' })
 
