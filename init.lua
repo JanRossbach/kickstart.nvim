a49a1f0fb848282acdfbb7072f3617254fd8b86d
JanRossbach <janrossbach3@gmail.com>
2025-03-26 09:37:56 +0100

    add harper ls

--- init.lua
+++ init.lua
@@ -900,6 +900,35 @@ require('lazy').setup({
             },
           },
         },
+        harper_ls = {
+          settings = {
+            ['harper-ls'] = {
+              userDictPath = '',
+              fileDictPath = '',
+              linters = {
+                SpellCheck = true,
+                SpelledNumbers = false,
+                AnA = true,
+                SentenceCapitalization = true,
+                UnclosedQuotes = true,
+                WrongQuotes = false,
+                LongSentences = true,
+                RepeatedWords = true,
+                Spaces = true,
+                Matcher = true,
+                CorrectNumberSuffix = true,
+              },
+              codeActions = {
+                ForceStable = false,
+              },
+              markdown = {
+                IgnoreLinkTitle = false,
+              },
+              diagnosticSeverity = 'hint',
+              isolateEnglish = false,
+            },
+          },
+        },
         rust_analyzer = {
           settings = {
             ['rust_analyzer'] = {
