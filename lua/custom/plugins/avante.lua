1d722ca21d10dae9e42cb85d851962bbcfdcdb5a
JanRossbach <janrossbach3@gmail.com>
2025-03-26 09:37:46 +0100

    update avante ollama provider

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -5,34 +5,9 @@ return {
   version = false, -- set this if you want to always pull the latest change
   opts = {
     provider = 'ollama',
-    use_absolute_path = true,
-    vendors = {
-      ---@type AvanteProvider
-      ollama = {
-        -- ['local'] = true,
-        api_key_name = '',
-        endpoint = 'http://localhost:11434/v1',
-        model = 'deepseek-r1:8b',
-        parse_curl_args = function(opts, code_opts)
-          return {
-            url = opts.endpoint .. '/chat/completions',
-            headers = {
-              ['Accept'] = 'application/json',
-              ['Content-Type'] = 'application/json',
-              ['x-api-key'] = 'ollama',
-            },
-            body = {
-              model = opts.model,
-              messages = require('avante.providers').copilot.parse_messages(code_opts), -- you can make your own message, but this is very advanced
-              max_tokens = 2048,
-              stream = true,
-            },
-          }
-        end,
-        parse_response_data = function(data_stream, event_state, opts)
-          require('avante.providers').openai.parse_response(data_stream, event_state, opts)
-        end,
-      },
+    ollama = {
+      endpoint = 'http://127.0.0.1:11434', -- Note that there is no /v1 at the end.
+      model = 'qwq',
     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
